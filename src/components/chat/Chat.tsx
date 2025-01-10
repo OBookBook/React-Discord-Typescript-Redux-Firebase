9cbd4e68bf3ed708d5701c957cf5d2970b1ba7aa
N.I <iworksWeb@gmail.com>
2025-01-10 13:24:20 +0900

    Implement chat input functionality and styles: Add input form with icons for sending messages, and enhance Chat.scss with new styles for input area and icons.

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,28 @@
 import "./Chat.scss";
 import ChatHeader from "./ChatHeader";
+import GifIcon from "@mui/icons-material/Gif";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
       <div className="chat__message"></div>
-      <div className="chat__input"></div>
+      <div className="chat__input">
+        <AddCircleIcon />
+        <form>
+          <input type="text" placeholder="add message" />
+          <button type="submit">Send</button>
+        </form>
+
+        <div className="chat_icons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   );
 }
