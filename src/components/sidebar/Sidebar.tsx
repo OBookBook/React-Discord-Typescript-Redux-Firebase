6e01c6cb34a42cf423ae21e69eb6e0270972ca91
N.I <iworksWeb@gmail.com>
2025-01-10 09:17:49 +0900

    Implement Sidebar component with updated styles and icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,10 +2,21 @@ import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <div className="sidebar__left">sidebarLeft</div>
-      <div className="sidebar__right">sidebarRight</div>
-    </div>
+    <aside className="sidebar">
+      <div className="sidebar__left">
+        <div className="sidebar__icon">
+          <img src="./../../../public/vite.svg" alt="server icon" />
+        </div>
+        <div className="sidebar__icon">
+          <img src="./../../../public/vite.svg" alt="server icon" />
+        </div>
+      </div>
+      <div className="sidebar__right">
+        <div className="sidebar__top">
+          <h3 className="sidebar__title">Discord</h3>
+        </div>
+      </div>
+    </aside>
   );
 };
 
