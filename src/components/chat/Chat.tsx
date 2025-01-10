f2265ca00319d8a58e03abb746f97f5d6625484f
N.I <iworksWeb@gmail.com>
2025-01-10 11:45:16 +0900

    Implement Chat component and update Sidebar styles: Add Chat component to App and adjust Sidebar flex properties for improved layout

---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,13 @@
+import "./Chat.scss";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header"></div>
+      <div className="chat__message"></div>
+      <div className="chat__input"></div>
+    </div>
+  );
+}
+
+export default Chat;
