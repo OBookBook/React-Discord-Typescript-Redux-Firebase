b01df0a84844a8c0f862e980bba556116642f26e
N.I <iworksWeb@gmail.com>
2025-01-11 09:14:07 +0900

    Implement message input functionality in Chat component: Add state management for input text and handle message sending on button click, enhancing user interaction in the chat interface.

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,5 @@
 import "./Chat.scss";
+import { useState } from "react";
 import ChatHeader from "./ChatHeader";
 import ChatMessage from "./ChatMessage";
 import GifIcon from "@mui/icons-material/Gif";
@@ -8,7 +9,12 @@ import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 function Chat() {
+  const [inputText, setInputText] = useState<string>("");
   const channelName = useAppSelector((state) => state.channel.channelName);
+
+  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="chat">
@@ -22,8 +28,21 @@ function Chat() {
       <div className="chat__input">
         <AddCircleIcon />
         <form>
-          <input type="text" placeholder="add message" />
-          <button type="submit">Send</button>
+          <input
+            type="text"
+            placeholder="add message"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+              sendMessage(e);
+            }}
+          >
+            Send
+          </button>
         </form>
 
         <div className="chat_icons">
